68c3d710a5e4dbec44e2c687cb4fd6d385e10cdf
JuanCaceffo <caceffojuan@gmail.com>
2023-03-05 14:59:58 -0300

    first commit

---
+++ bin/practica_juguetes_dart.dart
@@ -0,0 +1,5 @@
+import 'package:practica_juguetes_dart/practica_juguetes_dart.dart' as practica_juguetes_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practica_juguetes_dart.calculate()}!');
+}
---
+++ test/practica_juguetes_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:practica_juguetes_dart/practica_juguetes_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
