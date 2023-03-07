9e768b821fa622e2e48a30ffea83394347b9d97f
JuanCaceffo <caceffojuan@gmail.com>
2023-03-06 22:34:34 -0300

    finalizacion de la primera parte

--- bin/practica_juguetes_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:practica_juguetes_dart/practica_juguetes_dart.dart' as practica_juguetes_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${practica_juguetes_dart.calculate()}!');
-}
--- lib/practica_juguetes_dart.dart
+++ lib/practica_juguetes_dart.dart
@@ -1,3 +1,50 @@
-int calculate() {
-  return 6 * 7;
+class Toys{
+  double? _cuv = 2;
+  
+  void set setCuv(double cuv){
+    _cuv = cuv;
+  }
+}
+
+class Ballons extends Toys{
+  final double? _radio;
+  static const int mulradio = 3;
+
+  Ballons(this._radio);
+
+  //getter del radio
+  double? get getRadio => _radio;
+  
+  //costo del juguete
+  double price() => mulradio*_radio!*_cuv!; 
+}
+class Piece extends Toys{
+  double? _volumen;
+
+  Piece(this._volumen);
+
+  double? get getVolumen => _volumen!;
+  //con el signo de admiracion ! nos lanza una expcion si la variable contiene un valor nulo y con el de ? te retorna un valor null.
+  double price() => _cuv!*_volumen!;
+}
+
+class Baldecito extends Toys{
+  List<Piece>? _pieceOfTheToy;
+
+  Baldecito(this._pieceOfTheToy);
+  //getters
+  int cantPieces() => _pieceOfTheToy!.length;
+
+  int fixedPrice() => 5;
+  //this method returns the piece whit the higher volume 
+  Piece pieceHigherVolume() => _pieceOfTheToy!.reduce((curr, next) => curr.getVolumen!>next.getVolumen! ? curr:next);
+
+  double price() => fixedPrice()+(cantPieces()*pieceHigherVolume().price());
+}
+class TachitosApilables extends Baldecito{
+
+  TachitosApilables(List<Piece> PieceOfTheToy):super(PieceOfTheToy);
+  
+  @override
+  int fixedPrice() => 3;
 }
