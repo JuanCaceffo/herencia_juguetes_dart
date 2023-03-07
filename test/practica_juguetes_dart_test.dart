9c1fe92acbef0f6e5f22d2a40929196bc331bf37
JuanCaceffo <caceffojuan@gmail.com>
2023-03-07 15:26:13 -0300

    agregado de test para los dos niños

--- test/practica_juguetes_dart_test.dart
+++ test/practica_juguetes_dart_test.dart
@@ -1,4 +1,5 @@
 import 'package:practica_juguetes_dart/practica_juguetes_dart.dart';
+import 'package:practica_juguetes_dart/niñes.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -9,44 +10,62 @@ void main() {
   final part3 = Piece(5);
   final tachitos1 = TachitosApilables([part1,part2,part3]);
   final baldecito1 = Baldecito([part1,part2,part3]);
-  
-  group("test para pelota", (){
-    test("la pelota tiene un costo de 24",(){
-      expect(pelota1.cost(), 24);
-    });
-    test("la eficacia de la pelota es siempre de 12",(){
-      expect(pelota1.eficacia(), 12);
-    });
-    test("pelota tiene que tener un precio de 144",(){
-      expect(pelota1.price(), 144);
-    });
-  }); 
-  group("test para tachitos1", (){
-    test("el costo de tachitos1 es de 33", (){
-      expect(tachitos1.cost(), 33);
-    });
-    test("tachito1 tiene una eficacia de 11",(){
-      expect(tachitos1.eficacia(), 11);
+  final ninieNormie = NinieTipique([pelota1,baldecito1], 10);
+  final ninieTravieso = NinieRevoltose([pelota1,tachitos1], 10);
+  group("test jugetes", (){
+    group("test para pelota", (){
+      test("la pelota tiene un costo de 24",(){
+        expect(pelota1.cost(), 24);
+      });
+      test("la eficacia de la pelota es siempre de 12",(){
+        expect(pelota1.eficacia(), 12);
+      });
+      test("pelota tiene que tener un precio de 144",(){
+        expect(pelota1.price(), 144);
+      });
     }); 
-    test("tachito1 tiene que tener un precio de 164", (){
-      expect(tachitos1.price(),163);
+    group("test para tachitos1", (){
+      test("el costo de tachitos1 es de 33", (){
+        expect(tachitos1.cost(), 33);
+      });
+      test("tachito1 tiene una eficacia de 11",(){
+        expect(tachitos1.eficacia(), 11);
+      }); 
+      test("tachito1 tiene que tener un precio de 164", (){
+        expect(tachitos1.price(),163);
+      });
+    });
+    group("test para baldecito1",() {
+      test("la pieza mas grande del baldecito1 es part3",(){
+        expect(baldecito1.pieceHigherVolume(), part3);
+      });
+      test("el baldecito1 debe tener 3 piezas", (){
+        expect(3, baldecito1.cantPieces());
+      });
+      test("el baldecito1 tiene un costo de 35", (){
+        expect(baldecito1.cost(), 35);
+      });
+      test("valdecito1 tiene una eficacia de 9", (){
+        expect(baldecito1.eficacia(), 9);
+      });
+      test("baldecito1 tiene un precio de 145", (){
+        expect(baldecito1.price(),145);
+      });
     });
   });
-  group("test para baldecito1",() {
-    test("la pieza mas grande del baldecito1 es part3",(){
-      expect(baldecito1.pieceHigherVolume(), part3);
+  group("test ninies", (){
+    group("test ninie tipique", (){
+      test("coeficiente del ninieNormie es 1.1", (){
+        expect(ninieNormie.coeficiente(), 1.1);
+      });
+      test("ninieNormie debe ser entretenido por 13.1 minutos", (){
+        expect(ninieNormie.entreain(pelota1), 13.2);
+      });
     });
-    test("el baldecito1 debe tener 3 piezas", (){
-      expect(3, baldecito1.cantPieces());
-    });
-    test("el baldecito1 tiene un costo de 35", (){
-      expect(baldecito1.cost(), 35);
-    });
-    test("valdecito1 tiene una eficacia de 9", (){
-      expect(baldecito1.eficacia(), 9);
-    });
-    test("baldecito1 tiene un precio de 145", (){
-      expect(baldecito1.price(),145);
+    group("test ninies revoltoses", (){
+      test("el niniotravieso con la pelota1 es entretenido por 19.6 minutos", (){
+        expect(ninieTravieso.entreain(pelota1), 19.8);
+      });
     });
   });
 }
