9cf07425adfcd4bf3af888095ce7850ef80d10a2
JuanCaceffo <caceffojuan@gmail.com>
2023-03-06 22:34:55 -0300

    agreagado de test de baldecito1

--- test/practica_juguetes_dart_test.dart
+++ test/practica_juguetes_dart_test.dart
@@ -2,7 +2,21 @@ import 'package:practica_juguetes_dart/practica_juguetes_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  //class instances
+  final part1 = Piece(4);
+  final part2 = Piece(2);
+  final part3 = Piece(5);
+  final baldecito1 = Baldecito([part1,part2,part3]);
+  
+  group("test para baldecito1",() {
+    test("la pieza mas grande del baldecito1 es part3",(){
+      expect(baldecito1.pieceHigherVolume(), part3);
+    });
+    test("el baldecito1 debe tener 3 piezas", (){
+      expect(3, baldecito1.cantPieces());
+    });
+    test("el baldecito1 tiene un precio de 35", (){
+      expect(baldecito1.price(), 35);
+    });
   });
 }
