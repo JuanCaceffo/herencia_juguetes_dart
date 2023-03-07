a3d1efcf10234a3f19e8178de795afe4d3b624fc
JuanCaceffo <caceffojuan@gmail.com>
2023-03-07 13:35:52 -0300

    agregado de test de ballon y tachitos, testeamos punto 2

--- test/practica_juguetes_dart_test.dart
+++ test/practica_juguetes_dart_test.dart
@@ -3,11 +3,29 @@ import 'package:test/test.dart';
 
 void main() {
   //class instances
+  final pelota1 = Ballons(4);
   final part1 = Piece(4);
   final part2 = Piece(2);
   final part3 = Piece(5);
+  final tachitos1 = TachitosApilables([part1,part2,part3]);
   final baldecito1 = Baldecito([part1,part2,part3]);
   
+  group("test para pelota", (){
+    test("la pelota tiene un costo de 24",(){
+      expect(pelota1.cost(), 24);
+    });
+    test("la eficacia de la pelota es siempre de 12",(){
+      expect(pelota1.eficacia(), 12);
+    });
+  }); 
+  group("test para tachitos1", (){
+    test("el costo de tachitos1 es de 33", (){
+      expect(tachitos1.cost(), 33);
+    });
+    test("tachito1 tiene una eficacia de 11",(){
+      expect(tachitos1.eficacia(), 11);
+    }); 
+  });
   group("test para baldecito1",() {
     test("la pieza mas grande del baldecito1 es part3",(){
       expect(baldecito1.pieceHigherVolume(), part3);
@@ -15,8 +33,11 @@ void main() {
     test("el baldecito1 debe tener 3 piezas", (){
       expect(3, baldecito1.cantPieces());
     });
-    test("el baldecito1 tiene un precio de 35", (){
+    test("el baldecito1 tiene un costo de 35", (){
       expect(baldecito1.cost(), 35);
+    });
+    test("valdecito1 tiene una eficacia de 9", (){
+      expect(baldecito1.eficacia(), 9);
     });
   });
 }
